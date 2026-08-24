159220c24ef09478e66ab5f8a276da4fc3099563
SovaLunox <ariesguim07@gmail.com>
2026-08-24 12:01:44 +0800

    Create pdf-preview.blade.php

---
+++ resources/views/filament/modals/pdf-preview.blade.php
@@ -0,0 +1,7 @@
+<div style="width: 100%; height: 80vh;">
+    <iframe
+        src="{{ $url }}"
+        style="width: 100%; height: 100%; border: none; border-radius: 8px;"
+        type="application/pdf"
+    ></iframe>
+</div>
